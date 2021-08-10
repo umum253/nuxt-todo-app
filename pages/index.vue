4b82d9989995c989f942b81c10c37ce965656255
umum253 <fkwpostpost@gmail.com>
2021-08-10 14:36:22 +0900

    feat: 追加・検索・削除機能を追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,71 @@
 <template>
   <section class="container">
     <h1>Todo App</h1>
-    <p><input type="text" name="content"/></p>
+    <p><input type="text" name="content" v-model="content" @focus="set_flg" /></p>
     <div>
-      <button>save</button>
-      <button>find</button>
+      <button @click="insert">save</button>
+      <button @click="find">find</button>
     </div>
     <ul>
-      <li>
-        <span>hogehoge</span><span>(2021-01-01 15:00)</span><span>×</span>
+      <li v-for="(todo, index) in display_todos" :key="index">
+        <span>{{ todo.content }}</span><span>({{ todo.created }})</span><span @click="remove(todo)">×</span>
       </li>
     </ul>
   </section>
 </template>
 
 <script>
-export default {}
+import {mapState} from 'vuex';
+
+export default {
+  data: function() {
+    return {
+      content: '',
+      find_flg: false
+    }
+  },
+  // computed = 算出プロパティ
+  // v-modelと合わせて使うことで入力した数値が変化するごとに変数の中身をリアルタイムに変更することができる
+  // methodsとdataの中間的な役割。設定はmethodsと同じ書き方（function）呼び出しはdataと同じ書き方（カッコをつけない）
+  computed: {
+    ...mapState(['todos']),
+    // find_flgがtrueなら検索が一致したデータをarrに追加してarrを返す、find_flgがfalseならtodosすべてを返す
+    display_todos: function() {
+      if(this.find_flg) {
+        var arr = [];
+        var data = this.todos;
+        data.forEach(element => {
+          if(element.content.toLowerCase() == this.content.toLowerCase()) {
+            arr.push(element);
+          }
+        });
+        return arr;
+      } else {
+        return this.todos;
+      }
+    }
+  },
+  methods: {
+    // saveボタンをクリックしたとき、storeのmutationsのinsertに入力されたtodoを引数で渡して、追加処理を実行する
+    insert: function() {
+      this.$store.commit('insert', {content: this.content});
+      this.content = '';
+    },
+    // findボタンをクリックしたとき、find_flgをtrueに変更する
+    find: function() {
+      this.find_flg = true;
+    },
+    // 入力欄をクリックしたとき、find_flgをfalseに変更する
+    set_flg: function() {
+      if(this.find_flg) {
+        this.find_flg = false;
+        this.content = '';
+      }
+    },
+    // xをクリックしたとき、storeのmutationsのremoveにクリックされたtodoを引数で渡して、削除処理を実行する
+    remove: function(todo) {
+      this.$store.commit('remove', todo)
+    }
+  }
+}
 </script>
--- store/index.js
+++ store/index.js
@@ -1,36 +1,42 @@
 import Vuex from 'vuex';
 
 const createStore = () => {
-    return new Vuex.Store({
-        state: () => ({
-            todos: [
-                {content: 'hogehoge', created: '2021-03-31 15:30'},
-                {content: 'fugafuga', created: '2021-03-31 16:00'}
-            ]
-        }),
-        mutations: {
-            insert: function(state, obj) {
-                var date = new Date();
-                var fmt = date.getFullYear()
-                            + '-' + ('00' + (date.getMonth() + 1)).slice(-2)
-                            + '-' + ('00' + date.getDate()).slice(-2)
-                            + '-' + ('00' + date.getHours()).slice(-2)
-                            + '-' + ('00' + date.getMunites()).slice(-2);
-                state.todos.unshift({
-                    content: obj.content,
-                    creates: fmt
-                })
-            },
-            remove: function(state, obj) {
-                for(let i = 0; i < state.todos.length; i++) {
-                    const ob = state.todos[i];
-                    if(ob.content == obj.content && ob.created == obj.created) {
-                        alert('remove ' + '"' + ob.content + '"');
-                        state.todos.splice(i, 1);
-                        return;
-                    }
-                }
-            }
+  return new Vuex.Store({
+    // todosという配列を設定し、この中にtodoを追加したり削除したりする
+    // todoには内容(content)と追加された日時（created）の2つのプロパティを持たせる
+    state: () => ({
+      todos: [
+        {content: 'hogehoge', created: '2019-03-31 15:30'}, 
+        {content: 'fugafuga', created: '2019-03-31 16:00'}
+      ]
+    }),
+    mutations: {
+      // state（todos）と入力されたtodoを引数として、入力されたtodoと入力された時間を配列todosに追加する
+      insert: function(state, obj) {
+        var d = new Date();
+        var fmt = d.getFullYear() 
+                  + '-' + ('00' + (d.getMonth() + 1)).slice(-2) 
+                  + '-' + ('00' + d.getDate()).slice(-2) 
+                  + ' ' + ('00' + d.getHours()).slice(-2) 
+                  + ':' + ('00' + d.getMinutes()).slice(-2);
+        state.todos.unshift({
+          content: obj.content,
+          created: fmt
+        })
+      },
+      // state（todos）と入力されたtodoを引数として、入力と配列todosとが一致した場合に、alert表示＆配列todosから削除する
+      remove: function(state, obj) {
+        for(let i = 0; i < state.todos.length; i++) {
+          const ob = state.todos[i];
+          if(ob.content == obj.content && ob.created == obj.created) {
+            alert('remove ' + '"' + ob.content + '"');
+            state.todos.splice(i, 1);
+            return;
+          }
         }
-    })
+      }
+    }
+  })
 }
+
+export default createStore;
